e26a0bedb064599ed38fa69f07d70ecd74f39cc6
Richard Macaskill <richard.macaskill@neotechnology.com>
2023-04-14 17:19:51 +0100

    comments

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Identity.Client;
 using System.Net.Http.Headers;
 using Neo4j.Driver;
 using Neo4j.Driver.Experimental;
-using GraphDatabase =  Neo4j.Driver.GraphDatabase;
+using GraphDatabase = Neo4j.Driver.GraphDatabase;
 /*
 Notes;
     - requires allowing a redirect URI of http://localhost:58587 under 'Mobile and desktop applications'
@@ -21,18 +21,18 @@ namespace MSALNeo4jSample
     {
         private static PublicClientApplicationOptions? appConfiguration = null;
         private static IConfiguration? configuration;
-
-        // The MSAL Public client app
         private static IPublicClientApplication? application;
         private static string? Neo4j_URI;
+
+        // Entry point for the console application
         private static async Task Main(string[] args)
         {
-         var thistoken  = await BuildApplicationRequiringToken();
-         await ExecuteNeo4jQueryUsingToken(thistoken);
+            var thistoken = await BuildApplicationRequiringToken();
+            await ExecuteNeo4jQueryUsingToken(thistoken);
         }
-    private static async Task<String> BuildApplicationRequiringToken()
+        private static async Task<String> BuildApplicationRequiringToken()
         {
-               // Using appsettings.json for our configuration settings
+            // Using appsettings.json for our configuration settings
             var builder = new ConfigurationBuilder()
                 .SetBasePath(System.IO.Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
@@ -43,15 +43,15 @@ namespace MSALNeo4jSample
                 .Get<PublicClientApplicationOptions>();
 
             // We intend to obtain a token for Graph for the following scopes (permissions)
-            string[] scopes = new[] { string.Concat(appConfiguration.ClientId , "/.default") };
-            
+            string[] scopes = new[] { string.Concat(appConfiguration.ClientId, "/.default") };
+
             Neo4j_URI = configuration["Neo4jUri"];
 
             var mytoken = new AuthenticationHeaderValue("bearer", await SignInUserAndGetTokenUsingMSAL(appConfiguration, scopes));
 
             // Remove the "Bearer " section from the token string
-            return mytoken.ToString().Remove(0,7);
-          
+            return mytoken.ToString().Remove(0, 7);
+
         }
         private static async Task<string> SignInUserAndGetTokenUsingMSAL(PublicClientApplicationOptions configuration, string[] scopes)
         {
@@ -80,21 +80,20 @@ namespace MSALNeo4jSample
 
             return result.AccessToken;
         }
- private static async Task ExecuteNeo4jQueryUsingToken(String token)
- {
+        private static async Task ExecuteNeo4jQueryUsingToken(String token)
+        {
+            // create a connection to the Neo4j server using the token
+            var driver = GraphDatabase.Driver(Neo4j_URI, AuthTokens.Bearer(token));
 
-    var driver = GraphDatabase.Driver(Neo4j_URI,AuthTokens.Bearer( token));
+            var records = await driver
+                .ExecutableQuery("MATCH(m:Movie) return m.title as moviename;")
+                .WithConfig(new(database: "movies"))
+                .ExecuteAsync();
 
-    var records = await driver
-        .ExecutableQuery("MATCH(m:Movie) return m.title as moviename;")
-        .WithConfig(new (database: "movies"))
-        .ExecuteAsync();
+            // write out the list of movies to show it worked!
+           records.Result.ToList().ForEach(i => Console.WriteLine(i.Values["moviename"]));
 
-    // write out the list of movies to show it worked!
-
-    records.Result.ToList().ForEach(i => Console.WriteLine(i.Values["moviename"]));
-
- }
+        }
         /// <summary>
         /// Sign in user to AAD and obtain a token for Neo4j, then use it to authenticate and execute a query using the driver level query API
         /// </summary>
